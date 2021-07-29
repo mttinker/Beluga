527eb84222442a3aa070188980e020297b8f8319
Tim Tinker <ttinker@nhydra.comr>
2021-07-29 11:20:47 -0300

    Added data

--- Workspace.r
+++ Workspace.r
@@ -4,11 +4,11 @@
 M0 = matrix(0, nrow = 12,ncol = 12)
 # Note: stages 1:8 = ages 0-7 (both sexes), 9 = 8+ males, 
 #   10 = 8+ females "available", 11 = 8+ females preg, 12 = 8+ females w. calves 
-Sn = .7   # newborn survival (birth to time of survey, Sept 1)
-Sc = .8   # calf survival (from 0 to 1 yr old)
-Sy = .85   # yearling survival (1 to 2 year old)
-Sa = .95  # adult survival (annual survival for 2yr old and older)
-Pr = .9   # pregnancy rate (for "available" females)
+Sn = .83   # early newborn survival (birth to time of survey, Sept 1)
+Sc = .8    # calf survival (from 0.5 yr old to 1.5 yr old at next survey)
+Sy = .87   # yearling survival (1.5 to 2.5 year old)
+Sa = .95   # adult survival (annual survival for 2yr old and older)
+Pr = .9    # pregnancy rate (for "available" females)
 
 Make_matrix <- function(M0,Sn,Sc,Sy,Sa,Pr) {
   M = M0
@@ -36,7 +36,7 @@ lambda = eigs$values[1]
 W=abs(eigen(M)$vectors[,1]) # W=matrix of right eigenvectors 
 ssd = W/sum(W)              # stable stage distribution
 n = 1000 * ssd
-# annual demographic transitions accomplished with single matrix mult
+# annual demographic transitions accomplished with single matrix multiplication
 n = M %*% n
 # Use "Death Matrix" to create expected death assemblage for 9 age classes (0yr, 1:8+)
 DM = diag(1,9,9)*c(1-Sc,1-Sy,rep((1-Sa),7))
@@ -49,30 +49,28 @@ nd = DM %*% nt
 nd[1] = nd[1] + n[11] * Sa * (1-Sn)
 
 # ADDITIONAL NOTES:
-# - use beta distribution for proportion immatures in survey
-# - use multinomial (maybe Dirichlet-multinomial) for age comp of carcasses
+# - use beta distribution for observed proportion immature in survey
+# - use multinomial (maybe Dirichlet-multinomial?) for age comp of carcasses
 #   (relative prob of observing newborns reduced, maybe start as fixed ratio)
-# - log hazards (gamma) for adults/yearling/age0, incremental approach:
-#    - haz_A = exp(-5 + gamma_A + epsA)
-#    - haz_Y = exp(-5 + gamma_A + gamma_Y + epsA)
-#    - haz_0 = exp(-5 + gamma_A + gamma_Y + gamma_0 + eps0)
-# - age0 log hazards include stochastic hierarchical random effect (eps0))
-# - for yearling/adults, also stochastic? use scaled value of eps0, or independent epsA?
-# - preg rate stochastic also? may not be identifiable from newborn mortality...
-# - spit age0 hazards between newborn and calf survival, i.e. Sn = exp(-1*haz_0*fraction_nb)  
-# - add density dependence term to haz_0? or other covariates to pregnancy and survival? 
+# - log hazards (gamma) for adults/yearling/age0, use incremental approach
+#   where gmm0 ~ normal(0,1), gamma_A = gmm0 - 3, and each additional gamma term
+#   is a log hazard ratio required to be > 0 (half-normal prior):
+#    - haz_A = exp(gamma_A + eps_SA)
+#    - haz_Y = exp(gamma_A + gamma_Y + eps_S1)
+#    - haz_C = exp(gamma_A + gamma_Y + gamma_C + eps_S0)
+#     S_A = exp(-haz_A)   : survival for immatures/adults
+#     S_Y = exp(-haz_Y)   : survival for yearlings
+#     S_C = exp(-haz_C)   :survival for newborns, ~ 6mo to ~ 1.5 yrs (yearling) 
+#     S_N = exp(-haz_C/2) ; #(fractional mortality for newborns March to Sept) 
+# - age0 log hazards include stochastic hierarchical random effect (eps_S0),
+#    where eps0 ~ normal(0,1) and eps_S0 = eps0 * sig_S0
+# - stochasticity for yearlings may be lower and independent from age0, 
+#        so eps1 ~ normal(0,1), eps_S1 = eps1 * sig_S1 
+#       and eps_SA = eps1 * sig_SA (separate variance term)
+# - preg rate Pr = inv.logit(Beta0 - Beta1*N_t/1000 + eps_P), 
+#    where epsP ~ normal(0,1) and eps_P = epsP * sig_P
+# - add density-depend. term to preg AND haz_Y? other covariates for pregnancy/survival? 
 #  
-# Example of user function in STAN with vector (should also work with matrix):
-#  (the example function standardizes a vector variable to have a mean of 0 
-#    and standard deviation of 1, or just center it if scale=0.)
-# functions {
-#   vector stdized(int N, vector x, int scale) {
-#     vector[N] x_sc;
-#     
-#     x_sc = scale ? x-mean(x) : (x-mean(x))/sd(x);
-#     
-#     return x_sc;
-#   }
-# }
+
 
 
